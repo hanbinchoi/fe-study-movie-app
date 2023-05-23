a6159583a60e85b28ad7a0bf9bd1541594ab6c4e
hanbinchoi <wkghskak@naver.com>
2023-05-23 18:56:54 +0900

    1

--- client/src/CurrentMovie.js
+++ client/src/CurrentMovie.js
@@ -1,27 +1,33 @@
 // TODO: 하드코딩된 데이터를 동적으로 렌더링 되도록 다시 작성합니다.
 
-export default function CurrentMovie() {
+export default function CurrentMovie({ movie }) {
+  console.log(movie);
   return (
     <>
-      <div className='left-current-side'>
-        <div className='current-movie'>
-          <h1 className='title'>Avengers: Infinity War</h1>
-          <img
-            className='thumbnail'
-            src='https://yts.lt/assets/images/movies/avengers_infinity_war_2018/medium-cover.jpg'
-            alt='thumbnail'
-          />
-          <p className='rating'>rating : 8.5</p>
-          <p className='running-time'>runtime : 149min</p>
-          <p>description</p>
-          <p className='description'>
-            As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new
-            danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity
-            Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers
-            have fought for has led up to this moment, the fate of Earth and existence has never been more uncertain.
-          </p>
+      {movie === null ? (
+        <h1 className="title">영화를 선택하세요</h1>
+      ) : (
+        <div className="left-current-side">
+          <div className="current-movie">
+            <h1 className="title">{movie.title}</h1>
+            <img
+              className="thumbnail"
+              src={movie.medium_cover_image}
+              alt="thumbnail"
+            />
+            <p className="rating">
+              rating : {movie.rating}
+            </p>
+            <p className="running-time">
+              runtime : {movie.runtime}min
+            </p>
+            <p>description</p>
+            <p className="description">
+              {movie.description_full}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
