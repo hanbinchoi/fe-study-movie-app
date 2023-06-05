983b92faf8f00ff085b0a8b9eb3767a419e1a168
hanbinchoi <wkghskak@naver.com>
2023-06-05 18:55:11 +0900

    status

--- server/index.js
+++ server/index.js
@@ -4,25 +4,20 @@ const cors = require("cors");
 // HINT: 영화 데이터는 다음 movies 변수를 이용하세요
 const { movies } = require("./data.json");
 const app = express();
-// ✕ 모든 영화의 정보를 요청받은 경우, 모든 영화의 정보를 응답합니다. (12 ms)
-// ✓ 모든 영화의 정보를 요청받은 경우, 상태 코드 200을 응답합니다. (3 ms)
-// ✕ 특정 id에 대한 영화정보를 요청받은 경우, 해당하는 영화의 정보를 응답합니다. (2 ms)
-// ✕ 요청받은 영화의 id가 존재하지 않는경우, 상태 코드 404를 응답합니다. (1 ms)
-// ✓ 요청받은 영화의 id가 존재하는 경우, 상태 코드 200를 응답합니다. (1 ms)
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/movies", (req, res) => {
   // TODO:
-  return res.send(movies);
+  return res.status(200).send(movies);
 });
 
 app.get("/movies/:id", (req, res) => {
   // TODO:
   const { id } = req.params;
   const movie = movies.find((ele) => ele.id === Number(id));
-  if (movie) return res.send(movie);
+  if (movie) return res.status(200).send(movie);
   else return res.status(404).send("not found");
 });
 
